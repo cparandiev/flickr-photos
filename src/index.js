0d9ae7f775fd1d895c3a3e22175c797c449fc4a2
Tsvetko Parandiev <tsvetko.parandiev@ad.mentormate.bg>
2019-04-01 14:08:56 +0300

    add redux, router and browser history

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 
 import * as serviceWorker from './serviceWorker';
+import configureStore, { browserHistory } from './store/configureStore';
 import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(browserHistory);
+
+ReactDOM.render(
+  <Provider store={store}>   
+    <ConnectedRouter history={browserHistory}>
+      <App />
+    </ConnectedRouter> 
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/configureStore.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import { routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+
+import reducers from '../reducers';
+
+export const browserHistory = createBrowserHistory();
+
+export default (history) => {
+  const middlewares = applyMiddleware(
+      routerMiddleware(history),
+  );
+
+  const rootReducer = reducers(history);
+
+  const store = createStore(
+      rootReducer,
+      composeWithDevTools(middlewares)
+  );
+
+  return store;
+};
