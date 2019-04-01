c1d07c8f184a034f4f915975ba01cbe015b9afd6
Tsvetko Parandiev <tsvetko.parandiev@ad.mentormate.bg>
2019-04-01 16:38:58 +0300

    fixed the navbar on the top

--- src/features/Layout/Layout.jsx
+++ src/features/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+import './layout.css';
+
 export class Layout extends Component {
   render() {
     const { children } = this.props;
     
     return (
       <div className="main-layout">
-        <div className="container">
-            {children}
+        <div className="page-content">
+          {children}
         </div>
       </div>
     );
